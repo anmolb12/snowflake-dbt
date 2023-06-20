5ca45cbfbc3ffd2f95d7c041d3dbc81e52b15482
Anmol Bansal <anmolb1309@gmail.com>
2023-06-20 16:44:55 +0200

    Add terraform for snowflake

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.59"
+    }
+  }
+}
+
+provider "snowflake" {
+  role  = "SYSADMIN"
+}
+
+resource "snowflake_database" "db" {
+  name     = "TF_DEMO"
+}
+
+resource "snowflake_warehouse" "warehouse" {
+  name           = "TF_DEMO"
+  warehouse_size = "large"
+
+  auto_suspend = 60
+}
